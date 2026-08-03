a4064ef5c93148ac29a7f78e6e304b7d18a67bd0
Y.-H. Henry Chen <chenyh@mit.edu>
2026-08-03 11:08:18 -0400

    Update the cost function nesting structure

--- src/eppacore.jl
+++ src/eppacore.jl
@@ -56,15 +56,14 @@ function EPPACore(MGE, data, setting)
         @input(PM[i, r],                        data["xm0"][r, i, g],   s)  
     end)
 
-#    @production(MGE, D[g=data["set_i"], r=data["set_r"]], [t = 0, s = data["esub"][g], sn => s = data["esubn"][g], sve => sn = data["esubve"][g], sva => sve = data["esubva"][g], sef => sve = data["esubef"][g], sf => sef = data["esubf"][g]], begin
-    @production(MGE, D[g=data["set_i"], r=data["set_r"]], [t = 0, s = data["esub"][g]], begin
+    @production(MGE, D[g=data["set_i"], r=data["set_r"]], [t= 0, s = 0.3, nl => s = 0.3, nv => nl = 0.1, va => nv = 1, ne => nv = 0.1, nn => ne = 0.1, ee => ne = 1.5, fe => ee = 1.0], begin
         @output(PD[g, r],                       data["xp0"][r, g],      t,      taxes = [Tax(RA[r], td[r, g])],     reference_price = 1-data["rto0"][g, r])    
-        @input(PE[i=data["set_fe"], g, r],      data["xa0"][r, i, g],   s,      taxes = [Tax(RA[r], ta[i, g, r])],  reference_price = 1+data["ta0"][i, g, r])
-        @input(PE[i=data["set_elec"], g, r],    data["xa0"][r, i, g],   s,      taxes = [Tax(RA[r], ta[i, g, r])],  reference_price = 1+data["ta0"][i, g, r])
-        @input(PA[i=data["set_ne"], r],         data["xa0"][r, i, g],   s,      taxes = [Tax(RA[r], ta[i, g, r])],  reference_price = 1+data["ta0"][i, g, r])
+        @input(PE[i=data["set_fe"], g, r],      data["xa0"][r, i, g],   fe,     taxes = [Tax(RA[r], ta[i, g, r])],  reference_price = 1+data["ta0"][i, g, r])
+        @input(PE[i=data["set_elec"], g, r],    data["xa0"][r, i, g],   ee,     taxes = [Tax(RA[r], ta[i, g, r])],  reference_price = 1+data["ta0"][i, g, r])
+        @input(PA[i=data["set_ne"], r],         data["xa0"][r, i, g],   nn,     taxes = [Tax(RA[r], ta[i, g, r])],  reference_price = 1+data["ta0"][i, g, r])
         @input(PS[sf=data["set_fix"], g, r],    data["vfm"][sf, g, r],  s,      taxes = [Tax(RA[r], tf[sf, g, r])], reference_price = 1 + data["rtf0"][sf, g, r])    
-        @input(PS[sf=data["set_lnd"], g, r],    data["vfm"][sf, g, r],  s,      taxes = [Tax(RA[r], tf[sf, g, r])], reference_price = 1 + data["rtf0"][sf, g, r])    
-        @input(PF[mf=data["set_mf"], r],        data["vfm"][mf, g, r],  s,      taxes = [Tax(RA[r], tf[mf, g, r])], reference_price = 1 + data["rtf0"][mf, g, r])    
+        @input(PS[sf=data["set_lnd"], g, r],    data["vfm"][sf, g, r],  nl,     taxes = [Tax(RA[r], tf[sf, g, r])], reference_price = 1 + data["rtf0"][sf, g, r])    
+        @input(PF[mf=data["set_mf"], r],        data["vfm"][mf, g, r],  va,     taxes = [Tax(RA[r], tf[mf, g, r])], reference_price = 1 + data["rtf0"][mf, g, r])    
     end)
 
     for g ∈ data["set_con"], r ∈ data["set_r"]
@@ -107,9 +106,9 @@ function EPPACore(MGE, data, setting)
     end)
 
     @production(MGE, X[i=data["set_i"], s=data["set_r"], r=data["set_r"]], [t = 0, s = 0], begin
-        @output(PX[i, s, r], data["x0"][r, s, i], t)
-        @input(PD[i, s],                        data["wtflow0"][r, s, i], s,    taxes = [Tax(RA[s], -tx[i, s, r])],   reference_price = 1 - data["rtxs0"][i, s, r])
-        @input(PT[j=data["set_i"]],             data["vtwr"][j, i, s, r], s)
+        @output(PX[i, s, r], data["x0"][r, s, i],                       t)
+        @input(PD[i, s],                    data["wtflow0"][r, s, i],   s,    taxes = [Tax(RA[s], -tx[i, s, r])],   reference_price = 1 - data["rtxs0"][i, s, r])
+        @input(PT[j=data["set_i"]],         data["vtwr"][j, i, s, r],   s)
     end)
 
     @demand(MGE, RA[r=data["set_r"]], begin
